0d73047905a8beb3c95388f304bea23320c4cbaa
joshegladwell <joshegladwell@gmail.com>
2024-08-11 19:45:48 -0400

    Transfer project websites into joshegladwell.github.io

--- render_site.sh
+++ render_site.sh
@@ -3,6 +3,12 @@
 # Start by rendering from quarto files
 quarto render .
 
-# Copy ads.txt into root directory
-cp ads.txt docs/ads.txt
+# Copy ads.txt into website's root directory
+cp assets/after_render_insert/ads.txt docs/ads.txt
 echo "ads.txt added to root directory"
+
+# Copy projects into website
+cp -r assets/after_render_insert/projects/decline-great-salt-lake docs/projects/
+echo "decline-great-salt-lake added to projects"
+cp -r assets/after_render_insert/projects/us-school-shootings docs/projects/
+echo "us-school-shootings added to projects"

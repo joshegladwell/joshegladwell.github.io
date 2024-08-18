f72e914805ef6ec5241d1c1a5c84e8aa294e13e9
joshegladwell <joshegladwell@gmail.com>
2024-08-18 09:13:34 -0400

    Open school shootings project in new tab

--- render_site.sh
+++ render_site.sh
@@ -7,6 +7,11 @@ quarto render .
 cp assets/after_render_insert/ads.txt docs/ads.txt
 echo "ads.txt added to root directory"
 
+# Edit link to school shootings project to open in new tab
+sed -i '' 's|<a href="https://joshegladwell.github.io/us-school-shootings/" class="quarto-grid-link">|<a target="_blank" rel="noopener noreferrer" href="https://joshegladwell.github.io/us-school-shootings/" class="quarto-grid-link">|' docs/projects/projects.html
+echo "us-school-shootings opens in new tab"
+
 # Copy projects into website
 # cp -rf assets/after_render_insert/projects/us-school-shootings docs/projects/
 # echo "us-school-shootings added to projects"
+
